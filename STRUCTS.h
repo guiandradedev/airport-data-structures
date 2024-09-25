fb716ee44e27c126d1cc140c380eb3b94dc99a10
LuigiZanon <lugazabugre@outlook.com>
2024-09-25 13:54:59 -0300

    feat Data (in procces)

--- STRUCTS.h
+++ STRUCTS.h
@@ -1,6 +1,5 @@
 #ifndef STRUCTS_H_INCLUDED
-#define STRUCTS_H_INCLUDED
-
+#define STRUCTS_H_INCLUDED// 
 // Structs
 typedef struct data {
     int hora, minuto;
@@ -11,6 +10,7 @@ typedef struct voo {
     Data previsao_chegada;
     int num_passageiros;
     int check_hora;
+    Data horario_chegada;
 } Voo;
 
 typedef struct no
@@ -20,28 +20,34 @@ typedef struct no
 }No;
 
 // Prototipos
-void imprimirVoo(Voo voo);
+void imprimirVoo(Voo voo, int check_hora);
 int compararHoras(Data hora1, Data hora2, int minutos);
 void printData(Data data);
 Data setHora(int hora, int minuto);
 Data gerarData(int hora_minima, int minuto_minimo);
 
 // Funcoes
-void imprimirVoo(Voo voo) {
+void imprimirVoo(Voo voo, int check_hora) {
     printf("Codigo: %s\n", voo.codigo);
     printf("Passageiros: %d\n", voo.num_passageiros);
     printf("Previsao de chegada: ");
     printData(voo.previsao_chegada);
+    if(voo.horario_chegada.hora != NULL && voo.horario_chegada.minuto != NULL) {
+        printf("Horario de chegada: ");
+        printData(voo.horario_chegada);
+    }
     printf("\n");
-    if(voo.check_hora != NULL){
-        printf("Sem check_hora");
-    }else{
-        if(voo.check_hora == -1){
-            printf("Pouso de emergencia");
-        }else if(voo.check_hora == 1){
-            printf("Pouso dentro do horario previsto");
+    if(check_hora != 0) {
+        if(voo.check_hora != NULL){
+            printf("Sem check_hora");
         }else{
-            printf("Pouso atrasado");
+            if(voo.check_hora == -1){
+                printf("Pouso de emergencia");
+            }else if(voo.check_hora == 1){
+                printf("Pouso dentro do horario previsto");
+            }else{
+                printf("Pouso atrasado");
+            }
         }
     }
 }
@@ -51,7 +57,7 @@ void imprimirVoo(Voo voo) {
 // }
 
 void printData(Data data) {
-    printf("%.2d:%.2d", data.hora, data.minuto);
+    printf("%.2d:%.2d\n", data.hora, data.minuto);
 }
 
 Data setHora(int hora, int minuto) {
@@ -62,11 +68,16 @@ Data setHora(int hora, int minuto) {
 }
 
 Data gerarData(int hora_minima, int minuto_minimo) {
-    if(hora_minima == 23) hora_minima = 0;
-    if(minuto_minimo == 59) hora_minima = 0;
+    if(minuto_minimo ==60) {
+        hora_minima++;
+        minuto_minimo = 0;
+    }
+    if(hora_minima == 24) {
+        hora_minima = 0;
+    }
 
-    int hora = rand() % (24 - hora_minima + 1) + hora_minima;
-    int minuto = rand() % (60 - minuto_minimo + 1) + minuto_minimo;
+    int hora = rand() % (24 - hora_minima) + hora_minima;
+    int minuto = rand() % (60 - minuto_minimo) + minuto_minimo;
 
     return setHora(hora, minuto);
 }
--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "stdio.h"
 #include "STRUCTS.h"
 #include "FILA.h"
 #include "PILHA.h"
@@ -12,7 +11,11 @@
 
 void inserir_voo(Fila *esperas, Fila *emergencias, int emergencia);
 
-void autorizar_pouso(Fila *esperas, Fila *emergencias, Fila *pousos);
+void autorizar_pouso(Fila *esperas, Fila *emergencias, Fila *pousos, Data hora_atual);
+
+void relatorio(Fila *esperas, Fila *emergencias);
+
+void voos_pousados(Fila *pousos);
 
 void header();
 
@@ -27,9 +30,10 @@ int main() {
 
     Data hora_atual = gerarData(0,0);
     
-
+    
 
     int op =0;
+
     do{
         menu();
         scanf("%d",&op);
@@ -42,11 +46,11 @@ int main() {
         
         //chama função autorizar pouso
         case 2:
-            autorizar_pouso(esperas,emergencias,pousos);
+            autorizar_pouso(esperas,emergencias,pousos,hora_atual);
             break;
 
         case 3:
-            
+            relatorio(esperas,emergencias);
             break;
         case 4:
         
@@ -54,7 +58,7 @@ int main() {
         
         //chama função imprimir fila para a fila pousos
         case 5:
-            imprimeFila(pousos);
+            voos_pousados(pousos);
             break;
         case 6:   
 
@@ -80,20 +84,20 @@ void menu(){
     printf("\n[2]-Autorizar voo");//completar função 
     vermelho();
     printf("\n[3]-Relatorio das aeronaves");//criar função
-    printf("\n[4]-Imprimir proxima aeronova a pousar");//criar função
+    printf("\n[4]-Imprimir proxima aeronave a pousar");//criar função
     resetcor();
     printf("\n[5]-Imprimir aeronaves pousadas");
     vermelho();
     printf("\n[6]-Simular o pouso dentro de um intervalo de tempo");//criar função 
     resetcor();
     printf("\n[7]-Finalizar o sistema");
-    printf("\nOpcao: ")
+    printf("\nOpcao: ");
 }
 
 void header() {
-    printf("\n----------\n");
+    printf("\n\t----------\n");
     printf("\tBlueSky\n");
-    printf("----------\n");
+    printf("\t----------\n");
 }
 
 void inserir_voo(Fila *esperas, Fila *emergencias, int emergencia) {
@@ -109,18 +113,12 @@ void inserir_voo(Fila *esperas, Fila *emergencias, int emergencia) {
 
     printf("Insira a quantidade de passageiros\n");
         scanf("%d", &voo.num_passageiros);
-    do{
-        printf("Insira a previsão de chegada\n");
-        printf("Hora (0-23): ");
-        scanf("%d", &data.hora);
-    }while(data.hora < 0 && data.hora > 24);
     
-    do{
-        printf("Minutos (0-59): ");
-        scanf("%d", &data.minuto);
-    }while(data.minuto < 0 && data.minuto > 60);
+    voo.previsao_chegada = gerarData(0,0);
+    voo.horario_chegada.hora = NULL;
+    voo.horario_chegada.minuto = NULL;
 
-    voo.previsao_chegada = data;
+    printData(voo.previsao_chegada);
     
     printf("Eh pouso de emergencia? %c\n", emergencia == 0 ? 'S' : 'N');
 
@@ -132,25 +130,46 @@ void inserir_voo(Fila *esperas, Fila *emergencias, int emergencia) {
 }
 
 //completar função
-void autorizar_pouso(Fila *esperas, Fila *emergencias, Fila *pousos) {
+void autorizar_pouso(Fila *esperas, Fila *emergencias, Fila *pousos, Data hora_atual) {
     Voo voo_removido;
     if(VaziaFila(emergencias)) {
         if(!VaziaFila(esperas)) {
             voo_removido = RetiraFila(esperas);
-            // verificar se ta atrasado ou nao
-            
-            
+            // verificar se ta atrasado ou nao7
         } else {
             printf("Nao existem voos em espera para pousar\n");
             return;
         }
-    
+
     } else {
         voo_removido = RetiraFila(emergencias);
         voo_removido.check_hora = -1;
     }
-    
+
+    voo_removido.horario_chegada.hora = hora_atual.hora;
+    voo_removido.horario_chegada.minuto = hora_atual.minuto + 10;
+
     printf("Voo removido!\n");
-    imprimirVoo(voo_removido);
+    imprimirVoo(voo_removido, 0);
     InsereFila(pousos, voo_removido);
+}
+
+void relatorio(Fila *esperas, Fila *emergencias) {
+    header();
+
+    printf("Voos em estado de emergencia:\n");
+    imprimeFila(emergencias, 0);
+    printf("Voos em lista de espera:\n");
+    imprimeFila(esperas, 0);
+}
+
+//void simular_voos(Fila*esperas, Fila*emergencias, Data*hora_atual){
+
+
+//}
+void voos_pousados(Fila *pousos) {
+    header();
+
+    printf("Voos que ja pousaram:\n");
+    imprimeFila(pousos,0);
 }
